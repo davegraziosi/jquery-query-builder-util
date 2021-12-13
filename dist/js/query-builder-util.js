ddfaa44d701d369f583920e8eb0a51262bc54157
dgraziosi <davide.graziosi@exprivia.com>
2021-12-13 15:36:04 +0100

    versione senza operatori enable_ic

--- dist/js/query-builder-util.js
+++ dist/js/query-builder-util.js
@@ -1204,11 +1204,13 @@
 
 		function getQueryBuilderOperators(options) {
 			var skipExtraOperators = options && options.skipExtraOperators;
-			var ops = [
+			var ops = [];
+			if (options.enable_ic) 
+			ops =[
 				{ type: 'equal_ic',                 nb_inputs: 1, multiple: false, enable_ic: true,                        apply_to: ['string'] },
-				{ type: 'equal',                 nb_inputs: 1, multiple: false, enable_ic: true,                        apply_to: ['number', 'datetime', 'boolean'] },
+				{ type: 'equal',                 nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['number', 'datetime', 'boolean'] },
 				{ type: 'not_equal_ic',             nb_inputs: 1, multiple: false, enable_ic: true,                        apply_to: ['string'] },
-				{ type: 'not_equal',             nb_inputs: 1, multiple: false, enable_ic: true,                        apply_to: ['number', 'datetime', 'boolean'] },
+				{ type: 'not_equal',             nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['number', 'datetime', 'boolean'] },
 				{ type: 'in',                    nb_inputs: 1, multiple: true,  enable_ic: true,  show_separator: true, apply_to: ['string', 'number', 'datetime'] },
 				{ type: 'not_in',                nb_inputs: 1, multiple: true,  enable_ic: true,  show_separator: true, apply_to: ['string', 'number', 'datetime'] },
 				{ type: 'less',                  nb_inputs: 1, multiple: false,                                         apply_to: ['number', 'datetime'] },
@@ -1227,6 +1229,29 @@
 				{ type: 'is_not_empty',          nb_inputs: 0, multiple: false,                                         apply_to: ['string'] },
 				{ type: 'is_null',               nb_inputs: 0, multiple: false,                                         apply_to: ['string', 'number', 'datetime', 'boolean'] },
 				{ type: 'is_not_null',           nb_inputs: 0, multiple: false,                                         apply_to: ['string', 'number', 'datetime', 'boolean'] }];
+			else 
+				ops =[
+					{ type: 'equal',                 nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string','number', 'datetime', 'boolean'] },
+					{ type: 'not_equal',             nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string','number', 'datetime', 'boolean'] },
+					{ type: 'in',                    nb_inputs: 1, multiple: true,  enable_ic: false,  show_separator: true, apply_to: ['string', 'number', 'datetime'] },
+					{ type: 'not_in',                nb_inputs: 1, multiple: true,  enable_ic: false,  show_separator: true, apply_to: ['string', 'number', 'datetime'] },
+					{ type: 'less',                  nb_inputs: 1, multiple: false,                                         apply_to: ['number', 'datetime'] },
+					{ type: 'less_or_equal',         nb_inputs: 1, multiple: false,                                         apply_to: ['number', 'datetime'] },
+					{ type: 'greater',               nb_inputs: 1, multiple: false,                                         apply_to: ['number', 'datetime'] },
+					{ type: 'greater_or_equal',      nb_inputs: 1, multiple: false,                                         apply_to: ['number', 'datetime'] },
+					{ type: 'between',               nb_inputs: 2, multiple: false,                                         apply_to: ['number', 'datetime'] },
+					{ type: 'not_between',           nb_inputs: 2, multiple: false,                                         apply_to: ['number', 'datetime'] },
+					{ type: 'begins_with',           nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string'] },
+					{ type: 'not_begins_with',       nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string'] },
+					{ type: 'contains',              nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string'] },
+					{ type: 'not_contains',          nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string'] },
+					{ type: 'ends_with',             nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string'] },
+					{ type: 'not_ends_with',         nb_inputs: 1, multiple: false, enable_ic: false,                        apply_to: ['string'] },
+					{ type: 'is_empty',              nb_inputs: 0, multiple: false,                                         apply_to: ['string'] },
+					{ type: 'is_not_empty',          nb_inputs: 0, multiple: false,                                         apply_to: ['string'] },
+					{ type: 'is_null',               nb_inputs: 0, multiple: false,                                         apply_to: ['string', 'number', 'datetime', 'boolean'] },
+					{ type: 'is_not_null',           nb_inputs: 0, multiple: false,                                         apply_to: ['string', 'number', 'datetime', 'boolean'] }];
+					
 			if (!skipExtraOperators) {
 				var sqlExtra = [
 					{ type: 'last_n_minutes',        nb_inputs: 1, multiple: false,                                         apply_to: ['datetime'] },
